a8e0afc46c0a80125fa63cd788de99e0d2ed3354
Lucas Soler <hola@lucassoler.com.ar>
2025-08-16 11:15:45 -0300

    removes boilerplate

--- webapp/app/routes/home.tsx
+++ webapp/app/routes/home.tsx
@@ -1,13 +1,12 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "SWStarter" },
+    { name: "description", content: "An app that returns results from the Start Wars API" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <div>Home</div>;
 }
