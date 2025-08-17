4715a5d3d0bd5b53c31172058396992d9d3621c9
Lucas Soler <hola@lucassoler.com.ar>
2025-08-17 03:36:37 -0300

    add dark variant for brand color and update button hover state

--- webapp/app/components/ui/button.tsx
+++ webapp/app/components/ui/button.tsx
@@ -4,14 +4,13 @@ import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "~/lib/utils";
 
-// TODO: there's a hover state color
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:bg-[#c4c4c4] [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
   {
     variants: {
       variant: {
         default:
-          "bg-brand text-white hover:bg-primary/90 uppercase font-bold w-full rounded-full",
+          "bg-brand text-white hover:bg-brand-dark uppercase font-bold w-full rounded-full cursor-pointer",
         destructive:
           "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
