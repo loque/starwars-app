46888ef6f5fd1bcf5099bc637625345a42f58878
Lucas Soler <hola@lucassoler.com.ar>
2025-08-17 02:45:11 -0300

    add axios dependency and implement API client with type definitions

---
+++ webapp/app/lib/api.ts
@@ -0,0 +1,45 @@
+import axios, { type AxiosInstance } from "axios";
+
+let _api: AxiosInstance | undefined;
+export function api() {
+  if (!_api) {
+    _api = axios.create({
+      baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+    });
+  }
+  return _api;
+}
+
+export type SearchType = "people" | "movies";
+export type PersonSummary = {
+  name: string;
+  uid: string;
+};
+export function isPersonSummary(
+  item: PersonSummary | MovieSummary,
+): item is PersonSummary {
+  return (item as PersonSummary).name !== undefined;
+}
+export type MovieSummary = {
+  uid: string;
+  title: string;
+};
+export type SearchResult = PersonSummary | MovieSummary;
+
+export type Person = {
+  uid: string;
+  name: string;
+  height: string;
+  gender: string;
+  birth_year: string;
+  eye_color: string;
+  hair_color: string;
+  mass: string;
+  movies: MovieSummary[];
+};
+export type Movie = {
+  uid: string;
+  title: string;
+  opening_crawl: string;
+  characters: PersonSummary[];
+};
