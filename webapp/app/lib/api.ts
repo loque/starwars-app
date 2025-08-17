b72e49bfe7679ac733c0b63786ad90744482edbe
Lucas Soler <hola@lucassoler.com.ar>
2025-08-17 16:34:05 -0300

    implement api methods so that consumers don't need to think about details

--- webapp/app/lib/api.ts
+++ webapp/app/lib/api.ts
@@ -1,14 +1,5 @@
 import axios, { type AxiosInstance } from "axios";
 
-let _api: AxiosInstance | undefined;
-export function api() {
-  if (!_api) {
-    const baseURL = `http://${process.env.VITE_API_HOST || "localhost"}:${process.env.API_PORT}`;
-    _api = axios.create({ baseURL });
-  }
-  return _api;
-}
-
 export type SearchType = "people" | "movies";
 export type PersonSummary = {
   name: string;
@@ -42,3 +33,46 @@ export type Movie = {
   opening_crawl: string;
   characters: PersonSummary[];
 };
+
+let _api: AxiosInstance | undefined;
+export function api() {
+  if (!_api) {
+    const baseURL = `http://${process.env.VITE_API_HOST || "localhost"}:${process.env.API_PORT}`;
+    _api = axios.create({ baseURL });
+  }
+  return _api;
+}
+
+type SearchParams = {
+  searchType: SearchType;
+  searchTerm: string;
+};
+
+export async function search({ searchType, searchTerm }: SearchParams) {
+  const searchProp = searchType === "people" ? "name" : "title";
+  return api().get<SearchResult[]>(`/${searchType}`, {
+    params: { [searchProp]: searchTerm },
+  });
+}
+
+export async function getMovie(movieId?: string) {
+  if (!movieId) return null;
+  try {
+    const res = await api().get<Movie>(`/movies/${movieId}`);
+    return res.data;
+  } catch (error) {
+    console.error(`Error fetching movie by id ${movieId}`, error);
+    return null;
+  }
+}
+
+export async function getPerson(personId?: string) {
+  if (!personId) return null;
+  try {
+    const res = await api().get<Person>(`/people/${personId}`);
+    return res.data;
+  } catch (error) {
+    console.error(`Error fetching person by id ${personId}`, error);
+    return null;
+  }
+}
